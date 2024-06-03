008ed7066cf650d5ee4f63e40144a63f8f60ec42
Nenenganjarwati15 <149997455+Nenenganjarwati15@users.noreply.github.com>
2024-06-02 21:00:07 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+function myFunction() {
+	document.getElementById("1").innerHTML = "Welcome to my page";
+	document.getElementById("2").innerHTML = "Neneng Anjarwati";
+	document.getElementById("3").innerHTML = "Halo, nama saya Neneng Anjarwati. saya lahir di Makassar dan tinggal di Makassar. Terima kasih sudah mau mengenal lebih jauh!";
+}
